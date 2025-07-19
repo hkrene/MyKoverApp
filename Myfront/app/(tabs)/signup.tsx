9cb0aa2caffec34567cbb70ab97def7212449020
tshims <bentshims21@gmail.com>
2025-07-19 18:46:17 +0200

    mis a jour de sign up sur main

--- Myfront/app/(tabs)/signup.tsx
+++ Myfront/app/(tabs)/signup.tsx
@@ -1,96 +1,238 @@
-import React from 'react';
-import { View, Text, TextInput, ScrollView, Alert, TouchableOpacity, Image, Platform,
-    TouchableWithoutFeedback,
-    Keyboard, KeyboardAvoidingView,
- } from 'react-native';
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  ScrollView,
+  Alert,
+  TouchableOpacity,
+  Image,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+  KeyboardAvoidingView,
+} from 'react-native';
 import { Link } from 'expo-router';
-import { useState } from 'react';
+import { FontAwesome, FontAwesome5, FontAwesome6 } from '@expo/vector-icons';
 
-export default function NotificationsScreen() {
+export default function SignUpScreen() {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [fullName, setFullName] = useState('');
-    const [email, setEmail] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [password, setPassword] = useState('');
+  const [fullNameError, setFullNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [phoneError, setPhoneError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
+  const [passwordVisible, setPasswordVisible] = useState(false);
 
 
+  const validateFullName = (value:string) => {
+    setFullName(value);
+    if (!value.trim()) {
+      setFullNameError("Le nom complet est obligatoire");
+    } else {
+      setFullNameError("");
+    }
+  };
 
-    const handleSubmit = ()=>{
-        if(!fullName){
-            Alert.alert("le nom complet est Obligatoire")
-            return
-        }
-        if(!email){
-            Alert.alert("l'Email est Obligatoire")
-            return
-        }
-        if(!phoneNumber){
-            Alert.alert("le telephone est Obligatoire")
-            return
-        }
-        if(!password){
-            Alert.alert("le mot de passe est Obligatoire")
-            return
-        }
-        return Alert.alert("Vous etes inscrit, bienvenue chez myKover+")
+  const validateEmail = (value:string) => {
+    setEmail(value);
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!value) {
+      setEmailError("L'email est obligatoire");
+    } else if (!emailRegex.test(value)) {
+      setEmailError("Format d'email invalide");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const validatePhone = (value:string) => {
+    setPhoneNumber(value);
+    const phoneRegex = /^(?:\+?\d{1,4}|0)\d{8,14}$/;
+    if (!value) {
+      setPhoneError("Le téléphone est obligatoire");
+    } else if (!phoneRegex.test(value)) {
+      setPhoneError("Format invalide. Exemple : +24382XXXXXXX ou 082XXXXXXX");
+    } else {
+      setPhoneError("");
+    }
+  };
+
+  const validatePassword = (value:string) => {
+    setPassword(value);
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    if (!value) {
+      setPasswordError("Le mot de passe est obligatoire");
+    } else if (!passwordRegex.test(value)) {
+      setPasswordError("Min. 8 caractères, 1 maj, 1 min, 1 chiffre");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  const handleSubmit = () => {
+    validateFullName(fullName);
+    validateEmail(email);
+    validatePhone(phoneNumber);
+    validatePassword(password);
+
+    if (
+      !fullName || fullNameError ||
+      !email || emailError ||
+      !phoneNumber || phoneError ||
+      !password || passwordError
+    ) {
+      Alert.alert("Formulaire invalide", "Veuillez corriger les erreurs");
+      return;
     }
 
+    Alert.alert("Vous êtes inscrit, bienvenue chez myKover+");
+  };
 
   return (
-   <KeyboardAvoidingView 
-    behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-    style={{ flex: 1 }}
-   >
-        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <ScrollView contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white',}}>
-                <View style={{alignItems:'center', justifyContent:'center', marginBottom:40}}>
-                    <Image source={require('@/assets/images/myKover+Logo.png')} style={{width: 150, height: 150}} />
-                    <Text style={{color:'gray', fontSize:24, fontWeight:'bold'}}>Sign up</Text>
-                </View>
-                <View>
-                    <View style={{marginTop:10, marginBottom:10, alignItems:'start'}}>
-                        <Text style={{fontSize:16, fontWeight:'semibold', color:'gray', opacity:0.8}}>
-                            Nom et Prenom <Text style={{color:'orange'}}>*</Text>
-                        </Text>
-                        <TextInput style={{borderWidth: 1, borderColor:'gray', opacity:0.7, borderRadius:10, width:350, height:40, padding:10, marginTop:5}} onChangeText={setFullName}/>
-                    </View>
-                    <View style={{marginTop:10, marginBottom:10, alignItems:'start'}}>
-                        <Text style={{fontSize:16, fontWeight:'semibold', color:'gray', opacity:0.8}}>
-                            Email <Text style={{color:'orange'}}>*</Text>
-                        </Text>
-                        <TextInput style={{borderWidth: 1, borderColor:'gray', opacity:0.7, borderRadius:10, width:350, height:40, padding:10, marginTop:5}} onChangeText={setEmail}/>
-                    </View>
-                    <View style={{marginTop:10, marginBottom:10, alignItems:'start'}}>
-                        <Text style={{fontSize:16, fontWeight:'semibold', color:'gray', opacity:0.8}}>
-                            Telephone <Text style={{color:'orange'}}>*</Text>
-                        </Text>
-                        <TextInput style={{borderWidth: 1, borderColor:'gray', opacity:0.7, borderRadius:10, width:350, height:40, padding:10, marginTop:5}} onChangeText={setPhoneNumber}/>
-                    </View>
-                    <View style={{marginTop:10, marginBottom:10, alignItems:'start'}}>
-                        <Text style={{fontSize:16, fontWeight:'semibold', color:'gray', opacity:0.8}}>
-                            Mot de passe <Text style={{color:'orange'}}>*</Text>
-                        </Text>
-                        <TextInput style={{borderWidth: 1, borderColor:'gray', opacity:0.7, borderRadius:10, width:350, height:40, padding:10, marginTop:5}} onChangeText={setPassword}/>
-                    </View>
-                    <View>
-                        <TouchableOpacity style={{backgroundColor:'#22B2DC', padding:10, borderRadius:10, width:350, height:40, alignItems:'center', marginTop:10}} onPress={handleSubmit}>
-                            <Text style={{color:'white', fontSize:16, fontWeight:'bold'}}>
-                                S'inscrire
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                    
-                </View>
-                <View style={{width:350}}>
-                    <View style={{marginTop:20, }}>
-                        <Text style={{color:'gray', fontSize:16,}}>
-                            Vous avez deja un compte ?   <Link href="/login" style={{color:'#22B2DC', fontWeight:'bold'}}>Se connecter</Link>
-                        </Text>
-                    </View>
-                </View>
-                
-            </ScrollView>
-       </TouchableWithoutFeedback> 
-    </KeyboardAvoidingView>  
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView contentContainerStyle={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+          <View style={{ alignItems: 'center', marginBottom: 40 }}>
+            <Image source={require('@/assets/images/myKover+Logo.png')} style={{ width: 150, height: 150 }} />
+            <Text style={{ color: 'gray', fontSize: 24, fontWeight: 'bold' }}>Sign up</Text>
+          </View>
+
+          {/* Nom complet */}
+          <View style={{ marginBottom: 10, width: 350 }}>
+            <Text style={{ fontSize: 16, color: 'gray' }}>
+              Nom et prénom <Text style={{ color: 'orange' }}>*</Text>
+            </Text>
+            <TextInput
+              style={{
+                borderWidth: 1,
+                borderColor: fullNameError ? 'red' : 'gray',
+                borderRadius: 10,
+                height: 40,
+                padding: 10,
+                marginTop: 5,
+              }}
+              onChangeText={validateFullName}
+              value={fullName}
+            />
+            {fullNameError ? <Text style={{ color: 'red', marginTop: 5 }}>{fullNameError}</Text> : null}
+          </View>
+
+          {/* Email */}
+          <View style={{ marginBottom: 10, width: 350 }}>
+            <Text style={{ fontSize: 16, color: 'gray' }}>
+              Email <Text style={{ color: 'orange' }}>*</Text>
+            </Text>
+            <TextInput
+              style={{
+                borderWidth: 1,
+                borderColor: emailError ? 'red' : 'gray',
+                borderRadius: 10,
+                height: 40,
+                padding: 10,
+                marginTop: 5,
+              }}
+              keyboardType="email-address"
+              onChangeText={validateEmail}
+              value={email}
+              autoCapitalize="none"
+            />
+            {emailError ? <Text style={{ color: 'red', marginTop: 5 }}>{emailError}</Text> : null}
+          </View>
+
+          {/* Téléphone */}
+          <View style={{ marginBottom: 10, width: 350 }}>
+            <Text style={{ fontSize: 16, color: 'gray' }}>
+              Téléphone <Text style={{ color: 'orange' }}>*</Text>
+            </Text>
+            <TextInput
+              style={{
+                borderWidth: 1,
+                borderColor: phoneError ? 'red' : 'gray',
+                borderRadius: 10,
+                height: 40,
+                padding: 10,
+                marginTop: 5,
+              }}
+              keyboardType="phone-pad"
+              onChangeText={validatePhone}
+              value={phoneNumber}
+              maxLength={15}
+            />
+            {phoneError ? <Text style={{ color: 'red', marginTop: 5 }}>{phoneError}</Text> : null}
+          </View>
+
+          {/* Mot de passe */}
+          <View style={{ marginBottom: 10, width: 350 }}>
+            <Text style={{ fontSize: 16, color: 'gray' }}>
+              Mot de passe <Text style={{ color: 'orange' }}>*</Text>
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                borderWidth: 1,
+                borderColor: passwordError ? 'red' : 'gray',
+                borderRadius: 10,
+                marginTop: 5,
+                paddingHorizontal: 10,
+                height: 40,
+              }}
+            >
+              <TextInput
+                style={{ flex: 1 }}
+                secureTextEntry={!passwordVisible}
+                onChangeText={validatePassword}
+                value={password}
+                placeholder="Mot de passe"
+              />
+              <TouchableOpacity onPress={() => setPasswordVisible(!passwordVisible)}>
+                <FontAwesome6
+                  name={passwordVisible ? 'eye-slash' : 'eye'}
+                  size={18}
+                  color="gray"
+                />
+              </TouchableOpacity>
+            </View>
+            {passwordError ? (
+              <Text style={{ color: 'red', marginTop: 5 }}>{passwordError}</Text>
+            ) : null}
+          </View>
+
+
+          {/* Bouton d'inscription */}
+          <TouchableOpacity
+            style={{
+              backgroundColor: '#22B2DC',
+              padding: 10,
+              borderRadius: 10,
+              width: 350,
+              height: 40,
+              alignItems: 'center',
+              marginTop: 10,
+            }}
+            onPress={handleSubmit}
+          >
+            <Text style={{ color: 'white', fontSize: 16, fontWeight: 'bold' }}>
+              S'inscrire
+            </Text>
+          </TouchableOpacity>
+
+          {/* Lien vers login */}
+          <View style={{ width: 350, marginTop: 20 , marginBottom:10}}>
+            <Text style={{ color: 'gray', fontSize: 16 }}>
+              Vous avez déjà un compte ? <Link href="/login" style={{ color: '#22B2DC', fontWeight: 'bold' }}>Se connecter</Link>
+            </Text>
+          </View>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
