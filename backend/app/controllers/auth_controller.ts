6a3e2316b208fd422479cbeb9947e82c11f3fb5f
tshims <bentshims21@gmail.com>
2025-07-25 14:35:14 +0200

    creation de la base de donnees

--- backend/app/controllers/auth_controller.ts
+++ backend/app/controllers/auth_controller.ts
@@ -1,9 +1,40 @@
 import type { HttpContext } from '@adonisjs/core/http'
+import User from '#models/user'
 
 export default class AuthController {
-    public async signup({}:HttpContext){
+    public async signup({request, response}:HttpContext){
+        const { fullName, email, password } = request.only(['fullName', 'email', 'password'])
+
+        const user = await User.create({
+        fullName,
+        email,
+        password,
+        })
+
+        return response.created({
+        id: user.id,
+        email: user.email,
+        fullName: user.fullName,
+        })
 
     }
+
+    public async login({request, response, auth}:HttpContext){
+            const { email, password } = request.only(['email', 'password'])
+            try {
+              const token = await auth.use('api').attempt(email, password)
+        
+              return {
+                type: token.type,
+                token: token.token,
+                expires_at: token.expiresAt,
+                user: auth.user,
+              }
+            } catch {
+              return response.unauthorized({ message: 'Email ou mot de passe invalide' })
+            }
+    }
+
 
 
 }
